4b71b0f923ea6ee043095cf7d7d0ca30d325d181
Kien <92088660+KienDuc24@users.noreply.github.com>
2023-09-24 23:12:34 +0700

    up

--- BTTH01_CSE485/BTTH01_CSE485_ex02.sql
+++ BTTH01_CSE485/BTTH01_CSE485_ex02.sql
@@ -1,37 +1,28 @@
+-- TẠO CSDL
 CREATE DATABASE BTTH01_CSE485;
 USE BTTH01_CSE485;
 
 CREATE TABLE tacgia (
-  ma_tgia INT UNSIGNED PRIMARY KEY,
-  ten_tgia VARCHAR(100),
-  hinh_tgia VARCHAR(100)
+    ma_tgia INT UNSIGNED PRIMARY KEY,
+    ten_tgia VARCHAR(100) NOT NULL,
+    hinh_tgia VARCHAR(100)
 );
 
 CREATE TABLE theloai (
-  ma_tloai INT UNSIGNED PRIMARY KEY,
-  ten_tloai VARCHAR(50)
+    ma_tloai INT UNSIGNED PRIMARY KEY,
+    ten_tloai VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE baiviet (
-  ma_bviet INT UNSIGNED PRIMARY KEY,
-  tieude VARCHAR(200),
-  ten_bhat VARCHAR(100),
-  ma_tloai INT UNSIGNED,
-  tomtat TEXT,
-  FOREIGN KEY (ma_tloai) REFERENCES theloai(ma_tloai)
-);
-
-CREATE TABLE noidung (
-  ma_ndung INT UNSIGNED PRIMARY KEY,
-  ma_bviet INT UNSIGNED,
-  noidung TEXT,
-  ngayviet DATETIME,
-  hinhanh VARCHAR(200),
-  FOREIGN KEY (ma_bviet) REFERENCES baiviet(ma_bviet)
-);
-
-CREATE TABLE users (
-  user_id INT UNSIGNED PRIMARY KEY,
-  username VARCHAR(50),
-  password VARCHAR(50)
+    ma_bviet INT UNSIGNED PRIMARY KEY,
+    tieude VARCHAR(200) NOT NULL,
+    ten_bhat VARCHAR(100) NOT NULL,
+    ma_tloai INT UNSIGNED NOT NULL,
+    tomtat TEXT NOT NULL,
+    noidung text,
+    ma_tgia INT UNSIGNED NOT NULL,
+    ngayviet DATETIME,
+    hinhanh VARCHAR(200),
+    FOREIGN KEY (ma_tgia) REFERENCES baiviet(ma_tgia),
+    FOREIGN KEY (ma_tloai) REFERENCES theloai(ma_tloai)
 );
