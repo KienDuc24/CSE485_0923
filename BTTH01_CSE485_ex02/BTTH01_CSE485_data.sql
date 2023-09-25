493138962462b3e9d3519f9baefd32a74d0ff226
Kien <92088660+KienDuc24@users.noreply.github.com>
2023-09-26 00:02:19 +0700

    up

---
+++ BTTH01_CSE485_ex02/BTTH01_CSE485_data.sql
@@ -0,0 +1,35 @@
+
+INSERT INTO `baiviet` (`ma_bviet`, `tieude`, `ten_bhat`, `ma_tloai`, `tomtat`, `noidung`, `ma_tgia`, `ngayviet`, `hinhanh`) VALUES
+(1, 'tuyetvoi', 'yeu em lam', 1, 'amazing', 'tinhyeutuoihoctro', 1, '0000-00-00 00:00:00', 'woaaaaa'),
+(2, 'tuyetvoi', 'yeu em lam', 2, 'amazing', 'tinhyeutuoihoctro', 2, '0000-00-00 00:00:00', 'woaaaaa'),
+(3, 'thuong nhau may nui cungtreo', 'anh yeu em', 3, 'amazing', 'tinhyeutuoihoctro', 3, '0000-00-00 00:00:00', 'woaaaaa'),
+(4, 'thương nhau chin bo lam muoi', 'yeu em lam', 4, 'amazing', 'tinhyeutuoihoctro', 4, '0000-00-00 00:00:00', 'woaaaaa'),
+(5, 'tuyetvoi', 'yeu em mai mai', 5, 'amazing', 'tinhyeutuoihoctro', 5, '0000-00-00 00:00:00', 'woaaaaa'),
+(6, 'anh thuong em nhat ma', 'yeu em lam', 6, 'amazing', 'tinhyeutuoihoctro', 6, '0000-00-00 00:00:00', 'woaaaaa'),
+(7, 'tuyetvoi', 'yeu em lam', 7, 'amazing', 'tinhyeutuoihoctro', 7, '0000-00-00 00:00:00', 'woaaaaa'),
+(8, 'tuyetvoi', 'yeu em nhieu lam', 8, 'amazing', 'tinhyeutuoihoctro', 8, '0000-00-00 00:00:00', 'woaaaaa'),
+(9, 'tuyetvoi', 'thuong em nhieu lam', 9, 'amazing', 'tinhyeutuoihoctro', 9, '0000-00-00 00:00:00', 'woaaaaa');
+
+INSERT INTO `tacgia` (`ma_tacgia`, `ten_tacgia`, `hinh_tacgia`) VALUES
+(1, 'Nhacvietplus', 'tuyetvoi'),
+(2, 'Zingmp3', 'tuyetvoi'),
+(3, 'Ming', 'tuyetvoi'),
+(4, '16Typh', 'tuyetvoi'),
+(5, 'Ho Ngoc Ha', 'tuyetvoi'),
+(6, 'Charlie Puth', 'tuyetvoi'),
+(7, 'Taylor Swift', 'tuyetvoi'),
+(8, 'Le Quyen', 'tuyetvoi'),
+(9, 'Dam Vinh Hung', 'tuyetvoi'),
+(10, 'Quang Le', 'tuyetvoi');
+
+INSERT INTO `theloai` (`ma_tloai`, `ten_tloai`) VALUES
+(1, 'Nhac tru tinh'),
+(2, 'Nhac cai luong'),
+(3, 'Nhac do'),
+(4, 'Nhac vang'),
+(5, 'Nhac dan gian'),
+(6, 'Nhac tinh yeu'),
+(7, 'Nhac nonstop'),
+(8, 'Nhac socala keo mut'),
+(9, 'Nhac rap'),
+(10, 'Nhac chill');
